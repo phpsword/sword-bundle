3ebfa8373a6733cbf3418b885adc879d4371ba05
William Arin <williamarin.dev@gmail.com>
2022-09-09 22:57:53 +0800

    test: add tests for AutoFileEnvVarProcessor

--- src/EnvVarProcessor/AutoFileEnvVarProcessor.php
+++ src/EnvVarProcessor/AutoFileEnvVarProcessor.php
@@ -10,11 +10,11 @@ final class AutoFileEnvVarProcessor implements EnvVarProcessorInterface
 {
     public function getEnv(string $prefix, string $name, \Closure $getEnv): string|array|bool|null
     {
-        if ($fileEnv = getenv($name . '_FILE')) {
+        if ($fileEnv = $getEnv($name . '_FILE')) {
             return rtrim(file_get_contents($fileEnv), "\r\n");
         }
 
-        if (($val = getenv($name)) !== false) {
+        if (($val = $getEnv($name)) !== false) {
             return $val;
         }
 
